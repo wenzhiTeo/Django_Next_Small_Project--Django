7ca0fba89c176b2fafca6b75b4fef871f387bd57
TEO WEN ZHI <99173738+wenzhiTeo@users.noreply.github.com>
2022-04-21 16:02:33 +0800

    add DRF view and url for Next

--- django/core/settings.py
+++ django/core/settings.py
@@ -37,8 +37,9 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "store",
     "mptt",
+    "rest_framework",
+    "store",
 ]
 
 MIDDLEWARE = [
@@ -126,3 +127,10 @@ MEDIA_ROOT = BASE_DIR / "media/"
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+
+REST_FRAMEWORK = {
+    "DEFAULT_PERMISSION_CLASSES": [
+        "rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly"
+    ]
+}
--- django/store/views.py
+++ django/store/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from rest_framework import generics
+
+from .models import Product
+from .serializers import ProductSerializer
+
+# from django.shortcuts import render
+
 
 # Create your views here.
+class ProductListView(generics.ListAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
